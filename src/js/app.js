42399f49eed380a78ef73674bfb11d697939919c
serNoName <user15b12n5lkuidso2@gmail.com>
2023-07-21 02:35:51 +0300

    new file:   dist/404.html 	new file:   dist/about-us.html 	modified:   dist/assets/css/style.min.css 	new file:   dist/assets/img/1.jpg 	new file:   dist/assets/img/2.jpg 	new file:   dist/assets/img/3.jpg 	new file:   dist/assets/img/categories/1.png 	new file:   dist/assets/img/categories/10.png 	new file:   dist/assets/img/categories/11.png 	new file:   dist/assets/img/categories/12.png 	new file:   dist/assets/img/categories/2.png 	new file:   dist/assets/img/categories/3.png 	new file:   dist/assets/img/categories/4.png 	new file:   dist/assets/img/categories/5.png 	new file:   dist/assets/img/categories/6.png 	new file:   dist/assets/img/categories/7.png 	new file:   dist/assets/img/categories/8.png 	new file:   dist/assets/img/categories/9.png 	new file:   dist/assets/img/ico/checkbox.svg 	new file:   dist/assets/img/ico/filter.svg 	new file:   dist/assets/img/ico/loupe.svg 	new file:   dist/assets/img/item.jpg 	deleted:    dist/assets/img/logo.png 	new file:   dist/assets/img/slider-image.jpg 	new file:   dist/assets/img/slider/slider-arrow.svg 	modified:   dist/assets/js/app.min.js 	new file:   dist/cabinet.html 	new file:   dist/catalog.html 	new file:   dist/contacts.html 	new file:   dist/faq.html 	new file:   dist/files/css/splide.min.css 	new file:   dist/files/js/splide.min.js 	modified:   dist/index.html 	new file:   dist/item copy.html 	new file:   dist/item.html 	new file:   dist/news-single.html 	new file:   dist/news.html 	new file:   dist/policy.html 	new file:   dist/reg.html 	new file:   dist/testimonials.html 	modified:   gulp/version.json 	new file:   src/404.html 	new file:   src/about-us.html 	new file:   src/cabinet.html 	new file:   src/catalog.html 	new file:   src/contacts.html 	new file:   src/faq.html 	new file:   src/files/css/splide.min.css 	new file:   src/files/js/splide.min.js 	modified:   src/html/footer.html 	modified:   src/html/head.html 	modified:   src/html/header.html 	modified:   src/html/pop-ups.html 	new file:   src/img/1.jpg 	new file:   src/img/2.jpg 	new file:   src/img/3.jpg 	new file:   src/img/categories/1.png 	new file:   src/img/categories/10.png 	new file:   src/img/categories/11.png 	new file:   src/img/categories/12.png 	new file:   src/img/categories/2.png 	new file:   src/img/categories/3.png 	new file:   src/img/categories/4.png 	new file:   src/img/categories/5.png 	new file:   src/img/categories/6.png 	new file:   src/img/categories/7.png 	new file:   src/img/categories/8.png 	new file:   src/img/categories/9.png 	new file:   src/img/ico/checkbox.svg 	new file:   src/img/ico/filter.svg 	new file:   src/img/ico/loupe.svg 	new file:   src/img/item.jpg 	deleted:    src/img/logo.png 	new file:   src/img/slider-image.jpg 	new file:   src/img/slider/slider-arrow.svg 	modified:   src/index.html 	new file:   src/item.html 	modified:   src/js/app.js 	new file:   src/news-single.html 	new file:   src/news.html 	new file:   src/policy.html 	new file:   src/reg.html 	modified:   src/scss/base/mixins.scss 	new file:   src/scss/base/other copy.scss 	modified:   src/scss/base/other.scss 	modified:   src/scss/base/ui.scss 	new file:   src/scss/blocks/404.scss 	new file:   src/scss/blocks/aside.scss 	new file:   src/scss/blocks/cabinet.scss 	modified:   src/scss/blocks/contacts.scss 	modified:   src/scss/blocks/footer.scss 	modified:   src/scss/blocks/pop-up.scss 	modified:   src/scss/blocks/slider.scss 	modified:   src/scss/media.scss 	modified:   src/scss/style.scss 	new file:   src/testimonials.html

--- src/js/app.js
+++ src/js/app.js
@@ -1,8 +1,4 @@
-import '../../node_modules/swiper/swiper-bundle.min.js'
-
 $(document).ready(function () {
-
-
     // pop-ups
     $(".popup-triger").on("click", function () {
         const target = $(this).data("target");
@@ -16,4 +12,98 @@ $(document).ready(function () {
         $("body").removeClass("block-scroll");
     });
 
+    //lazyload
+    function lazyLoadImages() {
+        const lazyloadImages = document.querySelectorAll("img.lazyload");
+
+        const observer = new IntersectionObserver((entries, observer) => {
+            entries.forEach((entry) => {
+                if (
+                    entry.isIntersecting &&
+                    entry.target.classList.contains("lazyload")
+                ) {
+                    entry.target.src = entry.target.dataset.src;
+                    entry.target.classList.remove("lazyload");
+                    observer.unobserve(entry.target);
+                }
+            });
+        });
+
+        lazyloadImages.forEach((image) => {
+            observer.observe(image);
+        });
+    }
+    function debounce(func, wait) {
+        let timeout;
+        return function () {
+            const context = this;
+            const args = arguments;
+            clearTimeout(timeout);
+            timeout = setTimeout(() => {
+                func.apply(context, args);
+            }, wait);
+        };
+    }
+
+    $(window).ready(debounce(lazyLoadImages, 0));
+    $(window).scroll(debounce(lazyLoadImages, 0));
+
+    //accordions
+    $(".faq__btn").on("click", function (e) {
+        const parent = $(this).parent();
+        e.preventDefault();
+
+        parent
+            .toggleClass("active")
+            .find(".faq__content")
+            .stop()
+            .slideToggle(300);
+    });
+
+    //tabs
+    $("[data-target-tab]").on("click", function () {
+        const target = $(`[data-tab="${$(this).data("target-tab")}"]`);
+
+        $("[data-target-tab]").removeClass("active");
+        $(this).addClass("active");
+
+        $("[data-tab]").hide().removeClass("active");
+        target.show().addClass("active");
+    });
+
+    //input
+    $(".input__field").on("focusin", function () {
+        const parent = $(this).parents(".input");
+
+        parent.addClass("focus").addClass("active");
+    });
+    $(".input__field").on("focusout", function () {
+        const parent = $(this).parents(".input");
+
+        if (!$(this).val().length) parent.removeClass("focus");
+        parent.removeClass("active")
+    });
+
+    $(".input__field").each(function () {
+        if($(this).val().length) $(this).parents('.input').addClass('focus');
+    });
 });
+// init Swiper
+if ($(".splide").length) {
+    new Splide(".splide", {
+        type: "loop",
+        perPage: 1,
+        speed: 1000,
+        gap: "32px",
+        autoplay: true,
+        interval: 4000,
+        lazyLoad: "nearby",
+        arrowPath:
+            "M2 13.4932C0.89543 13.4932 0 14.3886 0 15.4932C0 16.5977 0.89543 17.4932 2 17.4932V13.4932ZM67.4142 16.9074C68.1953 16.1263 68.1953 14.86 67.4142 14.079L54.6863 1.35103C53.9052 0.56998 52.6389 0.56998 51.8579 1.35103C51.0768 2.13208 51.0768 3.39841 51.8579 4.17946L63.1716 15.4932L51.8579 26.8069C51.0768 27.5879 51.0768 28.8543 51.8579 29.6353C52.6389 30.4163 53.9052 30.4163 54.6863 29.6353L67.4142 16.9074ZM2 17.4932H66V13.4932H2V17.4932Z",
+        breakpoints: {
+            640: {
+                perPage: 1,
+            },
+        },
+    }).mount();
+}
